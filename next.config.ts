da0ed0192cee301a9acdf51561eecdd1e583a9c9
prem <prem.mali@somaiya.edu>
2026-03-04 02:00:11 +0530

    chore: setup next.js export config for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "";
+if (isGithubActions) {
+  // Required only if not using a custom domain.
+  const repoName = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  repo = `/${repoName}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: repo,
+  assetPrefix: repo,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
